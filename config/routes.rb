606bdfbcb67c2371fa199cb2a7f083805152881c
TheNobleTruth <dav.pel@hotmail.com>
2022-04-04 12:52:07 -0400

    intervention tables done

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
   get "quotes", to: "quotes#new"
   post "quotes", to: "quotes#create"
 
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   get "commercial",  to: "home#commercial"
   get "residential", to: "home#residential"
-
+  get "interventions", to: "interventions#interventions"
 
   resources :synthesized_messages
   
